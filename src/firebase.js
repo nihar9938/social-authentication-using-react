c5dd7c73a971041caafb0c032bdf74422c6a2499
Nihar Mishra <niharmishra11@gmail.com>
2021-07-24 20:54:57 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import "firebase/auth";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDToJgGA_3AXAnuXoDIFi7OfMDBvvj3qzE",
-    authDomain: "social-app-ab312.firebaseapp.com",
-    projectId: "social-app-ab312",
-    storageBucket: "social-app-ab312.appspot.com",
-    messagingSenderId: "984769911314",
-    appId: "1:984769911314:web:2f267307dea5c69a9a96cb"
+    apiKey: "AE",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 firebase.initializeApp(firebaseConfig);
